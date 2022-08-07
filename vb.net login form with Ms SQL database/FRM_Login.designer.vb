93e0a822e75bba982bbddc4b7befe975cb053100
boxsanook <37097193+boxsanook@users.noreply.github.com>
2022-08-07 13:22:10 +0700

    Add files via upload

---
+++ vb.net login form with Ms SQL database/FRM_Login.designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FRM_Login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FRM_Login))
+        Me.panel1 = New System.Windows.Forms.Panel()
+        Me.pictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.linkpass = New System.Windows.Forms.LinkLabel()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.txtpass = New System.Windows.Forms.TextBox()
+        Me.txtuser = New System.Windows.Forms.TextBox()
+        Me.Puser = New System.Windows.Forms.Panel()
+        Me.PPass = New System.Windows.Forms.Panel()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnlogin = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.btnminimizar = New System.Windows.Forms.PictureBox()
+        Me.btncerrar = New System.Windows.Forms.PictureBox()
+        Me.TimeOTP = New System.Windows.Forms.Timer(Me.components)
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.panel1.SuspendLayout()
+        CType(Me.pictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnminimizar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btncerrar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'panel1
+        '
+        Me.panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(182, Byte), Integer))
+        Me.panel1.Controls.Add(Me.pictureBox3)
+        Me.panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.panel1.Location = New System.Drawing.Point(0, 0)
+        Me.panel1.Margin = New System.Windows.Forms.Padding(4)
+        Me.panel1.Name = "panel1"
+        Me.panel1.Size = New System.Drawing.Size(333, 406)
+        Me.panel1.TabIndex = 9
+        '
+        'pictureBox3
+        '
+        Me.pictureBox3.Image = CType(resources.GetObject("pictureBox3.Image"), System.Drawing.Image)
+        Me.pictureBox3.Location = New System.Drawing.Point(49, 95)
+        Me.pictureBox3.Margin = New System.Windows.Forms.Padding(4)
+        Me.pictureBox3.Name = "pictureBox3"
+        Me.pictureBox3.Size = New System.Drawing.Size(202, 163)
+        Me.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pictureBox3.TabIndex = 0
+        Me.pictureBox3.TabStop = False
+        '
+        'linkpass
+        '
+        Me.linkpass.ActiveLinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(122, Byte), Integer), CType(CType(204, Byte), Integer))
+        Me.linkpass.AutoSize = True
+        Me.linkpass.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.linkpass.LinkColor = System.Drawing.Color.DarkGray
+        Me.linkpass.Location = New System.Drawing.Point(705, 376)
+        Me.linkpass.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.linkpass.Name = "linkpass"
+        Me.linkpass.Size = New System.Drawing.Size(197, 21)
+        Me.linkpass.TabIndex = 10
+        Me.linkpass.TabStop = True
+        Me.linkpass.Text = "Forgot your password?"
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.ForeColor = System.Drawing.Color.White
+        Me.label1.Location = New System.Drawing.Point(421, 29)
+        Me.label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(421, 40)
+        Me.label1.TabIndex = 14
+        Me.label1.Text = "Login mini Pampers 2021"
+        '
+        'txtpass
+        '
+        Me.txtpass.BackColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(80, Byte), Integer))
+        Me.txtpass.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtpass.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtpass.ForeColor = System.Drawing.Color.Silver
+        Me.txtpass.Location = New System.Drawing.Point(358, 175)
+        Me.txtpass.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtpass.Name = "txtpass"
+        Me.txtpass.Size = New System.Drawing.Size(544, 25)
+        Me.txtpass.TabIndex = 12
+        Me.txtpass.Text = "Password"
+        '
+        'txtuser
+        '
+        Me.txtuser.BackColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(80, Byte), Integer))
+        Me.txtuser.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtuser.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtuser.ForeColor = System.Drawing.Color.Silver
+        Me.txtuser.Location = New System.Drawing.Point(358, 95)
+        Me.txtuser.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtuser.Name = "txtuser"
+        Me.txtuser.Size = New System.Drawing.Size(544, 25)
+        Me.txtuser.TabIndex = 11
+        Me.txtuser.Text = "Username"
+        '
+        'Puser
+        '
+        Me.Puser.BackColor = System.Drawing.Color.White
+        Me.Puser.Location = New System.Drawing.Point(358, 127)
+        Me.Puser.Name = "Puser"
+        Me.Puser.Size = New System.Drawing.Size(544, 2)
+        Me.Puser.TabIndex = 17
+        '
+        'PPass
+        '
+        Me.PPass.BackColor = System.Drawing.Color.White
+        Me.PPass.Location = New System.Drawing.Point(358, 207)
+        Me.PPass.Name = "PPass"
+        Me.PPass.Size = New System.Drawing.Size(544, 2)
+        Me.PPass.TabIndex = 18
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(53, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.Red
+        Me.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer))
+        Me.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(70, Byte), Integer), CType(CType(118, Byte), Integer), CType(CType(126, Byte), Integer))
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnExit.ForeColor = System.Drawing.Color.LightGray
+        Me.btnExit.Location = New System.Drawing.Point(690, 290)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(212, 49)
+        Me.btnExit.TabIndex = 19
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnlogin
+        '
+        Me.btnlogin.BackColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(53, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.btnlogin.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(85, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(127, Byte), Integer))
+        Me.btnlogin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer))
+        Me.btnlogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(70, Byte), Integer), CType(CType(118, Byte), Integer), CType(CType(126, Byte), Integer))
+        Me.btnlogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnlogin.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnlogin.ForeColor = System.Drawing.Color.LightGray
+        Me.btnlogin.Location = New System.Drawing.Point(358, 290)
+        Me.btnlogin.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnlogin.Name = "btnlogin"
+        Me.btnlogin.Size = New System.Drawing.Size(212, 49)
+        Me.btnlogin.TabIndex = 13
+        Me.btnlogin.Text = "Login"
+        Me.btnlogin.UseVisualStyleBackColor = False
+        '
+        'Timer1
+        '
+        '
+        'btnminimizar
+        '
+        Me.btnminimizar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnminimizar.Image = CType(resources.GetObject("btnminimizar.Image"), System.Drawing.Image)
+        Me.btnminimizar.Location = New System.Drawing.Point(867, 4)
+        Me.btnminimizar.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnminimizar.Name = "btnminimizar"
+        Me.btnminimizar.Size = New System.Drawing.Size(20, 18)
+        Me.btnminimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.btnminimizar.TabIndex = 16
+        Me.btnminimizar.TabStop = False
+        '
+        'btncerrar
+        '
+        Me.btncerrar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btncerrar.Image = CType(resources.GetObject("btncerrar.Image"), System.Drawing.Image)
+        Me.btncerrar.Location = New System.Drawing.Point(895, 4)
+        Me.btncerrar.Margin = New System.Windows.Forms.Padding(4)
+        Me.btncerrar.Name = "btncerrar"
+        Me.btncerrar.Size = New System.Drawing.Size(20, 18)
+        Me.btncerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.btncerrar.TabIndex = 15
+        Me.btncerrar.TabStop = False
+        '
+        'TimeOTP
+        '
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.ForeColor = System.Drawing.Color.White
+        Me.CheckBox1.Location = New System.Drawing.Point(358, 229)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(112, 22)
+        Me.CheckBox1.TabIndex = 21
+        Me.CheckBox1.Text = "Remember"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        Me.CheckBox1.Visible = False
+        '
+        'FRM_Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(80, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(923, 406)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.btnminimizar)
+        Me.Controls.Add(Me.btncerrar)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.PPass)
+        Me.Controls.Add(Me.Puser)
+        Me.Controls.Add(Me.panel1)
+        Me.Controls.Add(Me.linkpass)
+        Me.Controls.Add(Me.btnlogin)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.txtpass)
+        Me.Controls.Add(Me.txtuser)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "FRM_Login"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FRM_Login"
+        Me.panel1.ResumeLayout(False)
+        CType(Me.pictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnminimizar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btncerrar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents panel1 As Panel
+    Private WithEvents pictureBox3 As PictureBox
+    Private WithEvents btnminimizar As PictureBox
+    Private WithEvents btncerrar As PictureBox
+    Private WithEvents linkpass As LinkLabel
+    Private WithEvents label1 As Label
+    Private WithEvents txtpass As TextBox
+    Private WithEvents txtuser As TextBox
+    Friend WithEvents Puser As Panel
+    Friend WithEvents PPass As Panel
+    Private WithEvents btnExit As Button
+    Private WithEvents btnlogin As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TimeOTP As Timer
+    Friend WithEvents CheckBox1 As CheckBox
+End Class
---
+++ vb.net login form with Ms SQL database/FRM_Login.vb
@@ -0,0 +1,142 @@
+﻿Imports System.Net.Mail
+Imports System.Runtime.InteropServices
+Imports System.Text.RegularExpressions
+Imports BB_API_Framework
+
+Public Class FRM_Login
+    <DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
+
+    <DllImport("user32.DLL", EntryPoint:="SendMessage")>
+    Private Shared Sub SendMessage(ByVal hWnd As System.IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer)
+    End Sub
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles Me.MouseDown, panel1.MouseDown, pictureBox3.MouseDown
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112, &HF012, 0)
+    End Sub
+
+    Private Sub panel1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs)
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112, &HF012, 0)
+    End Sub
+
+    Private Sub txtuser_Enter(ByVal sender As Object, ByVal e As EventArgs) Handles txtuser.Enter
+        If txtuser.Text = "Username" Then
+            txtuser.Text = ""
+            txtuser.ForeColor = Color.LightGray
+            Puser.BackColor = Color.Orange
+        End If
+    End Sub
+
+    Private Sub txtuser_Leave(ByVal sender As Object, ByVal e As EventArgs) Handles txtuser.Leave
+        If txtuser.Text = "" Then
+            txtuser.Text = "Username"
+            txtuser.ForeColor = Color.Silver
+            Puser.BackColor = Color.White
+        Else
+            Puser.BackColor = Color.Green
+        End If
+    End Sub
+
+    Private Sub txtpass_Enter(ByVal sender As Object, ByVal e As EventArgs) Handles txtpass.Enter
+        If txtpass.Text = "Password" Then
+            txtpass.Text = ""
+            txtpass.ForeColor = Color.LightGray
+            txtpass.UseSystemPasswordChar = True
+            PPass.BackColor = Color.Orange
+        End If
+    End Sub
+
+    Private Sub txtpass_Leave(ByVal sender As Object, ByVal e As EventArgs) Handles txtpass.Leave
+        If txtpass.Text = "" Then
+            txtpass.Text = "Password"
+            txtpass.ForeColor = Color.Silver
+            PPass.BackColor = Color.White
+            txtpass.UseSystemPasswordChar = False
+        Else
+            PPass.BackColor = Color.Green
+        End If
+    End Sub
+
+
+    Private Sub btncerrar_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btncerrar.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub btnminimizar_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnminimizar.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+
+    Private Sub txtuser_KeyDown(sender As Object, e As KeyEventArgs) Handles txtuser.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            txtpass.Focus()
+            txtpass.SelectAll()
+        End If
+    End Sub
+
+    Private Sub txtpass_KeyDown(sender As Object, e As KeyEventArgs) Handles txtpass.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            btnlogin_Click(sender, e)
+        End If
+    End Sub
+    Private Sub btnlogin_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnlogin.Click
+        If txtuser.Text = "" Or txtuser.Text = "Username" Then
+            MessageBox.Show("You have not filled out Username")
+            txtuser.Focus()
+            Exit Sub
+        End If
+        If txtpass.Text = "" Or txtpass.Text = "Password" Then
+            MessageBox.Show("You have not filled out Password")
+            txtpass.Focus()
+            Exit Sub
+        End If
+
+        'If MySqlSetting.Login(txtuser.Text, EncyPassword) Then
+        '    Me.DialogResult = DialogResult.OK
+        'Else
+        '    MessageBox.Show("User or password is incorrect.")
+        'End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+        'Me.DialogResult = DialogResult.OK
+    End Sub
+
+    Private Sub linkpass_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles linkpass.LinkClicked
+
+    End Sub
+
+
+
+
+    Function IsValidEmailFormat(ByVal s As String) As Boolean
+        Return Regex.IsMatch(s, "^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$")
+    End Function
+    Public RegisterX As New DataTable
+    Public Sub RegisterXB()
+        RegisterX = New DataTable
+        RegisterX.Columns.Add(New DataColumn("username", GetType(String)))
+        RegisterX.Columns.Add(New DataColumn("password", GetType(String)))
+        RegisterX.Columns.Add(New DataColumn("hKey", GetType(String)))
+        RegisterX.Columns.Add(New DataColumn("uKey", GetType(String)))
+        RegisterX.Columns.Add(New DataColumn("email", GetType(String)))
+    End Sub
+    Dim EncyPassword As String
+
+    Dim TS_OTP_CountDown As New TimeSpan(0, 0, 45) 'ten seconds
+    Dim SW_OTP_count As New Stopwatch
+
+
+    Private Sub TimeOTP_Tick(sender As Object, e As EventArgs) Handles TimeOTP.Tick
+        If SW_OTP_count.Elapsed <= TS_OTP_CountDown Then
+            Dim toGo As TimeSpan = TS_OTP_CountDown - SW_OTP_count.Elapsed
+        Else
+            SW_OTP_count.Stop()
+            TimeOTP.Stop()
+            SW_OTP_count.Reset()
+        End If
+    End Sub
+End Class
